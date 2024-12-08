993573d21387c967160cf5fddd8089c199a95802
Smite Chow <xiaopengyou@live.com>
2024-12-08 09:34:20 +0000

    use EnvironmentPlugin

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -40,7 +40,7 @@ var webpackConfig = merge(baseWebpackConfig, {
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
-    new webpack.DefinePlugin(env_vars),
+    new webpack.EnvironmentPlugin(env_vars),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
